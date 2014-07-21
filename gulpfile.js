224a745e64230e2536a55fcbb6364b71d04ffccb
twstddev <twstd.dev@gmail.com>
2014-07-21 20:42:31 +0100

    Enhance gulpfile and add development task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,39 @@ var run_sequence = require( "run-sequence" );
 
 GLOBAL.plugins = require( "gulp-load-plugins" )();
 
+// a config object that contains main configuration
+// for the application.
+GLOBAL.config = {
+	hostname : "localhost",
+	port : 3000,
+	base : "app",
+	dist : "dist"
+};
+
+/**
+ * @brief Converts given path to a relative to the
+ * application path.
+ *
+ * @return String converted path.
+ */
+GLOBAL.get_application_path = function( path ) {
+	return path_helper.join( GLOBAL.config.base, path );
+};
+
+/**
+ * @brief Calms down thrown errors.
+ * Logs them instead of stopping gulp execution.
+ */
+GLOBAL.outpu_error = function( error ) {
+	console.log( error.message );
+};
+
 // load tasks from the tasks directory
 require( "gulp-load" )( gulp );
 gulp.loadTasks( __dirname );
+
+gulp.task( "development", function( done ) {
+	run_sequence(
+		done
+	);
+} );
